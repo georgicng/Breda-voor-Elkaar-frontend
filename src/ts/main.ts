cc9ded3c9aa30d0bf57cfbe3865d30e4811bcf7a
Luciano Nooijen <lucianonooijen@gmail.com>
2018-06-04 16:18:05 +0200

    Make Bootstrap work

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,4 +1,8 @@
+import * as $ from 'jquery';
+import '../../node_modules/bootstrap/js/dist/index.js';
 import '../sass/style.scss';
+
+
 
 const test = (): void => {
     console.log('Typescript added');
