94b6980f8f0253f4a8b0be543a7766d09ccd0565
Luciano Nooijen <lucianonooijen@gmail.com>
2018-06-04 18:04:31 +0200

    Fix TS errors

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,11 +1,19 @@
-import * as $ from 'jquery';
+declare global {
+    interface Window {
+        $: any;
+        jquery: any;
+    }
+}
+
+import jquery from 'jquery';
+window.$ = window.jquery = jquery;
 import '../../node_modules/bootstrap/js/dist/index.js';
 import '../sass/style.scss';
 
 
 
 const test = (): void => {
-    console.log('Typescript added');
+    console.log('Typescript added'); // tslint:disable-line
 };
 
 test();
